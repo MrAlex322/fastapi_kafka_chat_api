e628e7c363e994be9b7bba105519ca45b0772a1f
alex1331 <danilin999@bk.ru>
2024-04-15 11:40:46 +0300

    add test + update app + readme.md

--- app/domain/exception/messages.py
+++ app/domain/exception/messages.py
@@ -3,9 +3,15 @@ from dataclasses import dataclass
 from domain.exception.base import AplicationException
 
 @dataclass(eq=False)
-class TextTooLongError(AplicationException):
+class TitleTooLongException(AplicationException):
     text: str
     
     @property
     def message(self):
         return f'Текст "{self.text}" слишком длинный'
+    
+@dataclass(eq=False)
+class EmptyTextException(AplicationException):
+    @property
+    def message(self):
+        return f'Текст не может быть пустым'
--- app/domain/values/messages.py
+++ app/domain/values/messages.py
@@ -1,14 +1,26 @@
 from dataclasses import dataclass
 
 from domain.values.base import BaseValueObject
-from domain.exception.messages import TextTooLongError
+from domain.exception.messages import EmptyTextException, TitleTooLongException
 
 @dataclass(frozen=True)
 class Text(BaseValueObject):
+    value = str
+    def validate(self):
+        if not self.value:
+            raise EmptyTextException(self.value)
+        
+    def as_generic_type(self):
+        return str(self.value)
+
+@dataclass(frozen=True)
+class Title(BaseValueObject):
     value = str 
     def validate(self):
+        if not self.value:
+            raise EmptyTextException(self.value)       
         if len(self.value) > 255:
-            raise TextTooLongError(self.value)
+            raise TitleTooLongException(self.value)
         
     def as_generic_type(self):
         return str(self.value)
