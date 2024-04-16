a06550e91201841e2d4c0dd234253f41534262d3
alex1331 <danilin999@bk.ru>
2024-04-16 14:40:42 +0300

    added chat domain events

--- app/domain/entities/base.py
+++ app/domain/entities/base.py
@@ -1,7 +1,10 @@
-from abc import ABC
+from abc import ABC, ABCMeta
+from copy import copy
 from dataclasses import dataclass, field
 from datetime import datetime
 from uuid import uuid4
+
+from domain.events.base import BaseEvent
 
 @dataclass
 class BaseEntity(ABC):
@@ -16,5 +19,19 @@ class BaseEntity(ABC):
     def __hash__(self) -> int:
         return hash(self.oid)   
     
-    def __eq__(self, __value__: 'BaseEntity') -> bool:
-        return self.oid == __value__.oid
+    def __eq__(self, __value: 'BaseEntity') -> bool:
+        return self.oid == __value.oid
+    
+    _events: list[BaseEvent] = field(
+        default_factory=list,
+        kw_only=True,
+        )
+    
+    def register_event(self, event: BaseEvent) -> None:
+        self._events.append(event)
+    
+    def pull_events(self) -> None:
+        register_events = copy(self._events)
+        self._events.clear()
+        
+        return register_events
